6c6f6e9ed30e4836bfa15429162e2d4516020eea
Michael Bestas <mkbestas@lineageos.org>
2025-03-17 14:40:33 +0200

    fixup! Change TARGET_RECOVERY_UI_LIB to use fully qualified names in coral
    Change-Id: Ie0e77bd84e757593124a71ff34422ea682f595fb

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -79,7 +79,7 @@ TARGET_RECOVERY_WIPE := device/google/coral/recovery.wipe
 TARGET_RECOVERY_FSTAB := device/google/coral/fstab.hardware
 TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_RECOVERY_UI_LIB := \
-    //hardware/google/pixel/recovery:librecovery_ui_pixel
+    //hardware/google/pixel/recovery:librecovery_ui_pixel \
     libfstab
 
 # Enable chain partition for system.
